32bac947f818588716ada36a7ea3decad55cec9c
achebe <achebepeter94@gmail.com>
2022-04-30 19:50:28 +0100

    first migration

--- feed/models.py
+++ feed/models.py
@@ -1,4 +1,8 @@
 from django.db import models
 
 # Create your models here.
-class Post(models.Mo)
+class Post(models.Model):
+    text = models.CharField(max_length=240)
+
+    def __str__(self) -> str:
+        return self.text
